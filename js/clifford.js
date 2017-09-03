48cd525ab6b62607ba59cfae368a796df6ad3e98
Lucien Grondin <grondilu@yahoo.fr>
2017-09-03 22:31:03 +0200

    create type hiearchy for tokens

--- js/clifford.js
+++ js/clifford.js
@@ -1,20 +1,16 @@
 'use strict';
+// This code is derived from algebra.js (http://algebra.js.org)
 
-// some symbolic names for encapsulation
+// some symbolic names
 //
-const PLUS     = Symbol('addition');
-const MINUS    = Symbol('substraction/negation');
-const MULTIPLY = Symbol('multiplication');
-const DIVIDE   = Symbol('division');
-const POWER    = Symbol('exponentiation');
-const L_PAREN  = Symbol('left parenthesis');
-const R_PAREN  = Symbol('rigtht parenthesis');
-const EQUALS   = Symbol('equality/affectation');
-
-const IDENTIFIER = Symbol('identifier');
-const NUMBER     = Symbol('number');
-const PAREN      = Symbol('parenthesis');
-const OPERATOR   = Symbol('operator');
+const PLUS   = Symbol('addition');
+const MINUS  = Symbol('substraction/negation');
+const MULT   = Symbol('multiplication');
+const DIVIDE = Symbol('division');
+const POWER  = Symbol('exponentiation');
+const LPAREN = Symbol('left parenthesis');
+const RPAREN = Symbol('rigtht parenthesis');
+const EQUALS = Symbol('equality/affectation');
 
 const _                   = Symbol("underscore");
 const _skipnontokens      = Symbol("skip non-tokens");
@@ -22,6 +18,39 @@ const _process_digits     = Symbol("process digits");
 const _process_number     = Symbol("process number");
 const _process_operator   = Symbol("process operator");
 const _process_identifier = Symbol("process identifier");
+
+const ε          = Symbol('epsilon');
+const NUMBER     = Symbol('number');
+const IDENTIFIER = Symbol('identifier');
+
+// Token class and its children
+//
+class Token { constructor(pos) { this.pos = pos; } }
+class Identifier extends Token {
+    constructor(pos, name) { super(pos); this.name = name; }
+}
+class LiteralNumber extends Token {
+    constructor(pos, string) { super(pos); this.string = string; }
+}
+class Parenthesis extends Token {
+    constructor(pos, symbol) {
+        super(pos);
+        if (symbol === LPAREN || symbol === RPAREN) {
+            this.symbol = symbol;
+        } else { throw new Error("unexpected argument type"); }
+    }
+}
+class Operator extends Token {
+    constructor(pos, symbol) {
+        const operators = [
+            PLUS, MINUS, MULT, DIVIDE, POWER, EQUALS
+        ];
+        super(pos);
+        if (operators.map(op => symbol === op).reduce((a,b) => a || b)) {
+            this.symbol = symbol;
+        } else { throw new Error("unexpected argument type"); }
+    }
+}
 
 /*
   The lexer module is a slightly modified version of the handwritten lexer by
@@ -46,7 +75,7 @@ class Lexer {
         this.buflen = input.length;
     }
 
-    // private functions and attributes
+    // private functions
     static get [_]() {
         return {
             isalpha: c =>
@@ -63,8 +92,9 @@ class Lexer {
 
     // Get tokens from the current buffer. A token is an object with
     // the following properties:
-    // - type: name of the pattern that this token matched (taken from rules).
-    // - value: actual string value of the token.
+    // - type: symbolic name of the pattern that this token matched (taken from rules).
+    // - string: actual string value of the token.
+    // - symbol: symbol value if relevant.
     // - pos: offset in the current buffer where the token starts.
     *tokens() {
         for(
@@ -91,13 +121,12 @@ class Lexer {
             endpos < this.buflen &&
             Lexer[_].isalphanum(this.buf.charAt(endpos))
         ) { endpos++; }
-        let tok = {
-            type: IDENTIFIER,
-            value: this.buf.substring(this.pos, endpos),
-            pos: this.pos
-        };
+        let identifier = new Identifier(
+            this.pos,
+            this.buf.substring(this.pos, endpos)
+        );
         this.pos = endpos;
-        return tok;
+        return identifier;
     }
 
     [_process_digits](position) {
@@ -123,26 +152,25 @@ class Lexer {
                 "Decimal point without decimal digits at position " +
                 (endpos-1)
             );
-        } 
+        }
         //construct the NUMBER token
-        let tok = {
-            type: NUMBER,
-            value: this.buf.substring(this.pos, endpos),
-            pos: this.pos
-        };
+        let number = new LiteralNumber(
+            this.pos,
+            this.buf.substring(this.pos, endpos)
+        );
         this.pos = endpos;
-        return tok;
+        return number;
     }
 
     [_process_operator]() {
         const optable = {
             '+'  : PLUS,
             '-'  : MINUS,
-            '*'  : MULTIPLY,
+            '*'  : MULT,
             '/'  : DIVIDE,
             '**' : POWER,
-            '('  : L_PAREN,
-            ')'  : R_PAREN,
+            '('  : LPAREN,
+            ')'  : RPAREN,
             '='  : EQUALS
         }
         let substring = '', op, pos = this.pos;
@@ -153,16 +181,25 @@ class Lexer {
                 ]
             )
         ) { pos++; op = optable[substring]; }
-        if (op) {
-            let tok = {
-                type: op === L_PAREN || op === R_PAREN ? PAREN : OPERATOR,
-                value: op,
-                pos: this.pos
-            }
+        if (op === LPAREN || op === RPAREN) {
+            let paren = new Parenthesis(
+                this.pos,
+                op
+            );
             this.pos = pos;
-            return tok;
-        } else { throw new Error(
-            `unexpected token at pos ${pos}, substring is "${substring}"`); }
+            return paren;
+        } else if (op) {
+            let operator = new Operator(
+                this.pos,
+                op
+            );
+            this.pos = pos;
+            return operator;
+        } else {
+            throw new Error(
+                `unexpected token at pos ${pos}, substring is "${substring}"`
+            );
+        }
     }
 
     [_skipnontokens]() {
@@ -180,19 +217,94 @@ class Lexer {
 }
 
 class Parser {
-    constructor() {
-    }
 
     parse(input) {
         let lexer = new Lexer();
         lexer.input = input;
-        let tokens = lexer.input(input);
+        this.tokens = lexer.tokens();
+    }
+    update() { this.token_iteration = this.tokens.next(); }
+
+    match(symbol) {
+        if (this.token_iteration.done) { return symbol === ε; }
+        let token = this.token_iteration.value;
+        switch (symbol) {
+            case PLUS:
+                return (token instanceof Operator) && (token.symbol === PLUS);
+            case MINUS:
+                return (token instanceof Operator) && (token.symbol === MINUS);
+            case MULT:
+                return (token instanceof Operator) && (token.symbol === MULT);
+            case POWER:
+                return (token instanceof Operator) && (token.symbol === POWER);
+            case DIVIDE:
+                return (token instanceof Operator) && (token.symbol === DIVIDE);
+            case EQUALS:
+                return (token instanceof Operator) && (token.symbol === EQUALS);
+            case LPAREN:
+                return (token instanceof Parenthesis) && (token.symbol === LPAREN);
+            case RPAREN:
+                return (token instanceof Parenthesis) && (token.symbol === RPAREN);
+            case NUMBER:
+                return token instanceof LiteralNumber;
+            case IDENTIFIER:
+                return token instanceof Identifier;
+            default:
+                return false;
+        }
+
     }
 
+    parseExpr() { return this.parseExprRest(this.parseTerm); }
+    parseTerm() { return this.parseTermRest(this.parseFactor()); }
+    parseFactor() {
+        if (this.match(NUMBER)) {
+            var num = this.parseNumber();
+            this.update();
+            return num;
+        } else if (this.match(IDENTIFIER)) {
+            var id = new Expression(this.token_iteration.value.string);
+            this.update();
+            return id;
+        }
+    }
 }
-var lexer = new Lexer();
-lexer.input = "(foo * bar) ** 2";
 
-for (let token of lexer.tokens()) {
-    console.log(token);
+class Expression {
+    constructor(variable) {
+        this.constants = [];
+        if (typeof(variable) === 'string') {
+            this.terms = [new Term(new Variable(variable))];
+        }
+    }
 }
+
+class Variable {
+    constructor(variable) {
+        if (typeof(variable) === 'string') {
+            this.degree = 1;
+            this.variable = Symbol.for(variable);
+        } else {
+            throw new TypeError(
+                `Invalid Argument (${variable.toString()}):`+
+                "Variable initalizer must be of type String."
+            );
+        }
+    }
+}
+
+class Term {
+    constructor(variable) {
+        if (variable instanceof Variable) {
+            this.variables = [variable.copy()];
+        } else if (true) {
+        }
+    }
+}
+
+
+var parser = new Parser();
+parser.parse("3.14*(foo + x)");
+parser.update();
+console.log(parser.match(NUMBER));
+
