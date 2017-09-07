849eb05a481fa7a0782f1e32763dc1620ea37483
Lucien Grondin <grondilu@yahoo.fr>
2017-09-08 01:08:02 +0200

    show toString() output

--- js/test.js
+++ js/test.js
@@ -5,30 +5,38 @@ const examples = [
     '1+2',
     '2*(3+4)',
     '3.14e0',
+    '3.14*e0',
     'a',
     'a+b',
+    'a*b',
+    'a b',
+    'x*e1',
     '(a+b)**2',
+    '(a+b)∧c',
+    '(a*b)∧c',
+    'a*b∧c',
+    'x²',
     'a**b**c',
     'foo*(a-b)',
     '3.14*r**2',
     'u·v',
     'no·ni',
-    'x=pi/3',
-    'y = pi/5',
-    '355/113*x',
+    'pi = 355/113',
     '(a-b)(a+b)',
     '1+u∧v',
     'no∧ni',
     'e0**2',
     'a·b∧c',
+    'no + x*e1 + y*e2 + z*e3 + (x² + y² + z²)/2*ni'
 ];
+
 
 let parser = require('./clifford').parser,
     errors = 0;
 for (let example of examples) {
     try {
-        parser.parse(example);
-        console.log(`"${example}" parsed`);
+        let ast = parser.parse(example);
+        console.log(`"${example}" parsed as ${ast.simplify()}`);
     } catch (e) {
         errors++;
         console.log(`"${example}": ${e}`);
