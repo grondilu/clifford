54afcb4e1e14713dcd75eb94d61f070fb76cf50c
Lucien Grondin <grondilu@yahoo.fr>
2017-09-08 01:31:31 +0200

    actually do not predefine e0 and e4

--- js/clifford.js
+++ js/clifford.js
@@ -22,13 +22,13 @@ class Vector extends MultiVector {
     get grade() { return new Grade(this, 1); }
 }
 class NilpotentVector extends Vector {}
+class UnitVector extends Vector {}
+
 let no = new NilpotentVector('no'),
     ni = new NilpotentVector('ni'),
-    e0 = new Vector('e0'),
-    e1 = new Vector('e1'),
-    e2 = new Vector('e2'),
-    e3 = new Vector('e3'),
-    e4 = new Vector('e4');
+    e1 = new UnitVector('e1'),
+    e2 = new UnitVector('e2'),
+    e3 = new UnitVector('e3');
 
 class Vector3D extends Vector {
     constructor(x, y, z, name) {
@@ -116,7 +116,7 @@ class BinaryInternalOperator extends MultiVector {
         );
     }
     toString() {
-        let [left, right] = 
+        let [left, right] =
             [this.left, this.right].map(
                 element => this
                 .classesWithLowerOperatorPrecedence
@@ -443,7 +443,7 @@ Zs = [\\u0020\\u00A0\\u1680\\u2000-\\u200A\\u202F\\u205F\\u3000]
 
 module.exports = {
     parser: require('pegjs').generate(grammar),
-    no, ni, e0, e1, e2, e3, e4,
+    no, ni, e1, e2, e3,
     SymbolTable,
     MultiVector, Vector, Vector3D, ConformalPoint, Real, Fraction, Int, Grade,
     InnerProduct, Addition, Subtraction, OuterProduct,
